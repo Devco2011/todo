a0651b9aecf9bae76cab9bb02a91f284659d6444
Devco2011 <devco2011@gmail.com>
2021-03-03 10:48:59 -0600

    set up

---
+++ src/components/TodoList.vue
@@ -0,0 +1,18 @@
+<template>
+  <h3>ToDo List</h3>
+</template>
+
+<script>
+import { todoItems } from "../data";
+
+export default {
+  data() {
+    return {
+      todos: [...todoItems],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
