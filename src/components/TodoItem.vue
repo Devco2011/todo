f5215e878186f604fb3e84dc313e1894c76c2967
Devco2011 <devco2011@gmail.com>
2021-03-03 12:48:40 -0600

    finished project

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
-    <h2>{{ todo.name }}</h2>
-    <button>Mark Completed</button>
+    <h2 :class="{ completed: todo.complete }">{{ todo.name }}</h2>
+    <button @click="handleClick">{{ buttonText }}</button>
   </div>
 </template>
 
 <script>
 export default {
   props: ["todo"],
+
+  methods: {
+    handleClick() {
+      this.$emit("status-change", this.todo);
+    },
+  },
+
+  computed: {
+    buttonText() {
+      return this.todo.complete ? "Mark inclomplete" : "Mark Completed";
+    },
+  },
 };
 </script>
 
@@ -18,5 +30,9 @@ div {
   margin: 15px;
   padding: 10px;
   text-align: center;
+}
+.completed {
+  text-decoration: line-through;
+  text-decoration-color: tomato;
 }
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <h3>ToDo List</h3>
+    <h5>There are {{ itemsRemaining.length }} left to do today.</h5>
     <div v-for="item in todos" :key="item.id">
-      <todo-item :todo="item" />
+      <todo-item :todo="item" @status-change="handleStatusChange" />
     </div>
   </div>
 </template>
@@ -13,6 +14,20 @@ import TodoItem from "./TodoItem.vue";
 
 export default {
   components: { TodoItem },
+
+  methods: {
+    handleStatusChange(item) {
+      item.complete = !item.complete;
+      console.log(item);
+    },
+  },
+
+  computed: {
+    itemsRemaining() {
+      return this.todos.filter((t) => !t.complete);
+    },
+  },
+
   data() {
     return {
       todos: [...todoItems],
